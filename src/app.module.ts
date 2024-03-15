dc9228687947fdb498231cc9c44def467e96dd43
arshad2002 <62824836+arshad2002@users.noreply.github.com>
2024-03-15 16:24:19 +0600

    cleaning up customer branch

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ManagerModule } from './Inventory_Manager/manager.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { CategoryModule } from './Inventory_Manager/Category/category.module';
 import { CustomersModule } from './customers/customers.module';
 
 @Module({
-  imports: [CustomersModule,ManagerModule,CategoryModule,TypeOrmModule.forRoot({
+  imports: [CustomersModule,TypeOrmModule.forRoot({
     type: 'postgres',
     host: 'localhost',
     port: 5432,
